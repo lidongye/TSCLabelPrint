d8dffa68f4b54145f3eedb6a0d967cfcf17df1cf
zxcyzcs <897624129@qq.com>
2020-04-27 16:04:54 +0800

    test

--- labelPrint.py
+++ labelPrint.py
@@ -1,4 +1,5 @@
 import sys
+from datetime import date
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from winform import *
 import tsclib
@@ -8,24 +9,41 @@ class win(QMainWindow, Ui_MainWindow):
         # 初始化界面
         super(win, self).__init__(parent)
         self.setupUi(self)
+        self.radBtn1.setChecked(True)
         # 关联信号和槽
         self.btnPrint.clicked.connect(self.printSample)
 
     def printSample(self):
-        # 打印标签
-        self.initPrinter()
+        # 设置打印项目
+        testNum = 0
+        testList = [["HIV、RPR、TPPA"], ["HIV（复）"], ["HIV（复）", "EDTA（留）"], ["HIV、RPR、TPPA"], ["十三五亲和力"],
+                    ["HIV（复）", "EDTA(CD4)"]]
+        strDate = date.today().isoformat()
         if self.radBtn1.isChecked():
-            print("项目一")
+            testNum = 0
         if self.radBtn2.isChecked():
-            print("项目二")
+            testNum = 1
         if self.radBtn3.isChecked():
-            print("项目三")
+            testNum = 2
         if self.radBtn4.isChecked():
-            print("项目四")
+            testNum = 3
         if self.radBtn5.isChecked():
-            print("项目5")
+            testNum = 4
         if self.radBtn6.isChecked():
-            print(("项目六"))
+            testNum = 5
+        tests = testList[testNum]
+        self.initPrinter()
+        for test in tests:
+            tsclib.clearBuffer()
+            tsclib.windowsFont(10, 10, 40, 0, 0, 0, "宋体", "姓名：{}".format(self.edtName.text()))
+            tsclib.windowsFont(10, 60, 25, 0, 0, 0, "宋体", "项目：{}".format(test))
+            if testNum == 3:
+                tsclib.windowsFont(10, 90, 25, 0, 0, 0, "宋体", "十三五横断面")
+            tsclib.windowsFont(10, 120, 25, 0, 0, 0, "宋体", "采集时间：{}".format(strDate))
+            if testNum == 4:
+                tsclib.printLabel(1 , 4)
+            else:
+                tsclib.printLabel(1, 1)
         tsclib.closePrinter()
 
     def initPrinter(self):
