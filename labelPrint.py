e2aca56fb0a590a61193152179d9d5ca2d9453d3
zxcyzcs <897624129@qq.com>
2020-04-27 11:33:29 +0800

    test

--- labelPrint.py
+++ labelPrint.py
@@ -1,14 +1,15 @@
-import tsclib
-# 测试
-# 初始化打印机
-tsclib.openPrinter("TSC TTP-244M Pro")
-tsclib.setupPrinter(40, 20, 1.0, 8, 0, 2, 0)
-# tsclib.sendCommand("SIZE 40 mm, 20 mm".encode("gbk"))  # 设置纸张大小
-# tsclib.sendCommand("GAP 2mm, 0mm".encode("gbk"))  # 设置标签间距
-tsclib.sendCommand("DIRECTION 1".encode("gbk"))  # 设置打印方向
-tsclib.clearBuffer()
-# tsclib.windowsFont(20, 20, 40, 0, 0, 0, "宋体", "甄吴廖ABC")
-# tsclib.printFont(20, 20, "TSS24.BF2", 0, 1, 1, "甄吴廖")
-# tsclib.barCode(10, 10, "128", 35, 1, 0, 2, 2, "1234567890abcd")
-tsclib.printLabel(1, 1)
-tsclib.closePrinter()
+from winform import *
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+class win(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(win, self).__init__(parent)
+        self.setupUi(self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWin = win()
+    mainWin.show()
+    sys.exit(app.exec())
---
+++ winform.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'winform.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(20, 50, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(20, 20, 54, 12))
+        self.label.setObjectName("label")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 23))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "PushButton"))
+        self.label.setText(_translate("MainWindow", "哈哈"))
